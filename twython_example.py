d89a40369cdb7708ae2195943b8c089ea696ef8f
digitalmonkey-risd <44655420+digitalmonkey-risd@users.noreply.github.com>
2018-12-27 23:03:58 -0500

    Add files via upload
    Twitter post script

---
+++ twython_example.py
@@ -0,0 +1,19 @@
+from twython import Twython
+
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+twitter = Twython(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+twitter.update_status(status="Art of Going Viral")
+
+
